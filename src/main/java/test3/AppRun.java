6f91f087e450269aec65407678009ae3b5cc9ebb
xufangrong <xufr998@163.com>
2018-03-18 15:32:48 +0800

    测试

---
+++ src/main/java/test3/AppRun.java
@@ -0,0 +1,12 @@
+package test3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRun {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRun.class, args);
+    }
+}
